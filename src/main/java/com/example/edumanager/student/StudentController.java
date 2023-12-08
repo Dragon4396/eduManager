9e3da67f80143d2960353d00b7eb8dbb1a291e1c
Dragon4396 <hekunlong2515@gmail.com>
2023-12-08 00:13:58 -0800

    implement Flyway and Migrations

--- src/main/java/com/example/edumanager/student/StudentController.java
+++ src/main/java/com/example/edumanager/student/StudentController.java
@@ -1,29 +1,23 @@
 package com.example.edumanager.student;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
+@Data
 @RestController
 @RequestMapping("students")
 public class StudentController {
 
+    @Autowired
+    private final StudentService studentService;
+
     @GetMapping
     public List<Student> getAllStudents() {
-        return List.of(
-                new Student(UUID.randomUUID(),
-                        "James",
-                        "Bond",
-                        "jamesbond@gmail.com",
-                        Student.Gender.MALE),
-                new Student(UUID.randomUUID(),
-                        "Emily",
-                        "Smith",
-                        "emilySmith@gmail.com",
-                        Student.Gender.FEMALE)
-        );
+        return studentService.getAllStudents();
     }
 }
---
+++ src/main/java/com/example/edumanager/student/StudentService.java
@@ -0,0 +1,26 @@
+package com.example.edumanager.student;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class StudentService {
+
+    public List<Student> getAllStudents() {
+        return List.of(
+                new Student(UUID.randomUUID(),
+                        "James",
+                        "Bond",
+                        "jamesbond@gmail.com",
+                        Student.Gender.MALE),
+                new Student(UUID.randomUUID(),
+                        "Emily",
+                        "Smith",
+                        "emilySmith@gmail.com",
+                        Student.Gender.FEMALE)
+        );
+    }
+
+}
